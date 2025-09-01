a44932231977fd8b1298aeeca3c3ac4cf1020076
Jan Stuchlik <j.stuchlik@outlook.cz>
2025-09-01 11:22:14 +0200

    sample plugin

---
+++ SampleSol/ClassLibrary1/ExtensionSamples/Class1.cs
@@ -0,0 +1,14 @@
+﻿using EnvDTE80;
+using System.Windows.Forms;
+
+namespace ExtensionSamples
+{
+    public class Sample1
+    {
+        //change type of package, if you want to reference another libraries
+        public void Run(DTE2 dte, /*Microsoft.VisualStudio.Shell.Package*/ object package)
+        {
+            MessageBox.Show("Running!");
+        }
+    }
+}
