30e67297a3f96951785bb0751ae5e7dcc5cc0ccc
Jan Stuchlik <j.stuchlik@outlook.cz>
2025-09-18 13:51:09 +0200

    sample

---
+++ SampleSol/ClassLibrary1/ExtensionSamples/Sample3.cs
@@ -0,0 +1,18 @@
+﻿using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtensionSamples.Extra
+{
+    public class Sample3
+    {
+        public void Run(DTE dte)
+        {
+            MessageBox.Show("As simple as possible");
+        }
+    }
+}
