e141807d3b867b35a744d1e644349792b7be7989
Jan Stuchlik <j.stuchlik@outlook.cz>
2026-08-14 17:08:42 +0200

    samples

---
+++ SampleSol/ClassLibrary1/ExtensionSamples/Sample4.cs
@@ -0,0 +1,19 @@
+﻿using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionSamples
+{
+    internal class Sample4
+    {
+        //prop or field (instance or static), or out param?, NO const!
+        public string Extension => ".cs";
+        public string Generate(DTE dte, string input, string inputFileName,string ns)
+        {
+            return $"Output from solution generator: input file {inputFileName} length {input.Length}, namespace {ns}";
+        }
+    }
+}
